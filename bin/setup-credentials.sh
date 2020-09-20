fb79b924b8bda56e293732f8d18abd0a22867c4f
Yang Hanlin <mattoncis@hotmail.com>
2020-09-20 23:56:04 +0800

    fix: attempt to fix credential setup errors

--- bin/setup-credentials.sh
+++ bin/setup-credentials.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 "$_OSSUTIL" config \
-    --access-key-id="$OSS_ACCESSKEY_ID" \
-    --access-key-secret="$OSS_ACCESSKEY_SECRET" \
-    --endpoint="$OSS_ENDPOINT" \
-    --config-file="$_OSSUTIL_CONFIG_FILE" && \
+    --access-key-id "$OSS_ACCESSKEY_ID" \
+    --access-key-secret "$OSS_ACCESSKEY_SECRET" \
+    --endpoint "$OSS_ENDPOINT" \
+    --config-file "$_OSSUTIL_CONFIG_FILE" && \
 chmod 600 "$_OSSUTIL_CONFIG_FILE"
